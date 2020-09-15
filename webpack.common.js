0299f4c5bf048a9feacb65ce49bd71195534a758
Dimas Saputra <dimas@dicoding.com>
2020-09-15 14:04:58 +0700

    Configuring Webpack

---
+++ webpack.common.js
@@ -0,0 +1,113 @@
+const { resolve } = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackInjector = require('html-webpack-injector');
+const { GenerateSW } = require('workbox-webpack-plugin');
+const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
+const ImageminMozjpeg = require('imagemin-mozjpeg');
+
+const webcomponentsjs = './node_modules/@webcomponents/webcomponentsjs';
+
+const polyfills = [
+    {
+        from: resolve(`${webcomponentsjs}/webcomponents-bundle.js`),
+        to: 'vendor',
+        flatten: true,
+    },
+    {
+        from: resolve(`${webcomponentsjs}/webcomponents-bundle.js.map`),
+        to: 'vendor',
+        flatten: true,
+    },
+    {
+        from: resolve(`${webcomponentsjs}/webcomponents-loader.js`),
+        to: 'vendor',
+        flatten: true,
+    },
+    {
+        from: resolve(`${webcomponentsjs}/bundles/`),
+        to: 'vendor/bundles',
+        flatten: true,
+    },
+    {
+        from: resolve(`${webcomponentsjs}/custom-elements-es5-adapter.js`),
+        to: 'vendor',
+        flatten: true,
+    },
+];
+
+module.exports = {
+    entry: {
+        polyfill_head: resolve(__dirname, 'src/scripts/utils/polyfill.js'),
+        main: resolve(__dirname, 'src/scripts/index.js'),
+    },
+
+    output: {
+        filename: '[name].bundle.js',
+        path: resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css|\.s([ca])ss$/,
+                exclude: resolve(__dirname, 'src/styles'),
+                use: [{
+                    loader: 'lit-scss-loader',
+                    options: {
+                        minify: true,
+                    },
+                }, 'extract-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.css|\.s([ca])ss$/,
+                exclude: resolve(__dirname, 'src/scripts/presentations/components'),
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.js$/,
+                exclude: '/node_modules/',
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: resolve(__dirname, 'src/templates/index.html'),
+            filename: 'index.html',
+            inject: true,
+            chunks: ['main', 'polyfill_head'],
+        }),
+        new HtmlWebpackInjector(),
+        new CopyWebpackPlugin({
+            patterns: [
+                ...polyfills,
+                {
+                    from: resolve(__dirname, 'src/public/'),
+                    to: resolve(__dirname, 'dist/'),
+                },
+            ],
+        }),
+        new ImageminWebpackPlugin({
+            plugins: [
+                ImageminMozjpeg({
+                    quality: 50,
+                    progressive: true,
+                }),
+            ],
+        }),
+        new GenerateSW({
+            clientsClaim: true,
+            skipWaiting: true,
+            runtimeCaching: [
+                // runtime caching
+            ],
+        }),
+    ],
+};
---
+++ webpack.prod.js
@@ -0,0 +1,38 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const { merge } = require('webpack-merge');
+
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+    mode: 'production',
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+            minSize: 20000,
+            maxSize: 70000,
+            minChunks: 1,
+            maxAsyncRequests: 30,
+            maxInitialRequests: 30,
+            automaticNameDelimiter: '~',
+            enforceSizeThreshold: 50000,
+            cacheGroups: {
+                defaultVendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: -10,
+                },
+                default: {
+                    minChunks: 2,
+                    priority: -20,
+                    reuseExistingChunk: true,
+                },
+            },
+        },
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new BundleAnalyzerPlugin({
+            analyzerPort: 'auto',
+        }),
+    ],
+});
